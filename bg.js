26d60408660aa3d86290e269d66ff0a2d17f0218
karan1149 <karan1149@gmail.com>
2016-11-27 16:46:37 -0800

    Added code to get and store userID tokens.

---
+++ bg.js
@@ -0,0 +1,25 @@
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    return hex;
+}
+
+chrome.storage.sync.get('userid', function(items) {
+    var userid = items.userid;
+    if (userid) {
+        useToken(userid);
+    } else {
+        userid = getRandomToken();
+        chrome.storage.sync.set({userid: userid}, function() {
+            useToken(userid);
+        });
+    }
+    function useToken(userid) {
+        // TODO: Use user id for authentication or whatever you want.
+    }
+});
