6debedfa24ea220746db35c33c916ce6a237660d
karan1149 <karan1149@gmail.com>
2016-11-30 17:37:51 -0800

    Updated display text.

--- popup.js
+++ popup.js
@@ -11,16 +11,16 @@ showReal = function(){
 var analyzed = "We analyzed this website to see if it was similar to known fake news sites using a machine learning model. The same technology is used to power other artificial intelligence applications, like Siri and self-driving cars!";
 
   spinner.style.display = "none";
+
+  header.innerText = "Real!";
+  displayDiv.innerHTML = "<span>This site is probably not a fake news site.</span><br /><br />";
+  displayDiv.innerHTML += analyzed;
+
   displayDiv.className += " fadeIn";
   header.className += " fadeIn";
 
   body.style.backgroundColor = "#D9FFD9";
   body.style.color = "#004000";
-
-  header.innerText = "Not Fake!";
-  displayDiv.innerHTML = "This site is probably a reliable source for news.<br /><br />";
-
-  displayDiv.innerHTML += analyzed;
 
 
 };
@@ -32,16 +32,18 @@ showFake = function(){
   header = document.getElementById("headerText");
 
   spinner.style.display = "none";
+
+  header.innerText = "Fake!";
+  displayDiv.innerHTML = "<span>This site is probably not a reliable source for news.</span><br /><br />";
+  displayDiv.innerHTML += analyzed;
+
   displayDiv.className += " fadeIn";
   header.className += " fadeIn";
 
   body.style.backgroundColor = "#FFD9D9";
   body.style.color = "#730000";
 
-  header.innerText = "Fake!";
-  displayDiv.innerHTML = "This site is probably not a reliable source for news.<br /><br />";
 
-  displayDiv.innerHTML += analyzed;
 };
 
 function showError(errorMessage){
@@ -54,7 +56,7 @@ function showError(errorMessage){
 
   header.innerText = "Error!";
   if (!errorMessage){
-    displayDiv.innerHTML = "This site does not appear to be a valid news site.";
+    displayDiv.innerHTML = "<span>This site does not appear to be a valid news site.</span>";
   } else {
     displayDiv.innerHTML = errorMessage;
   }
